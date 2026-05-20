e522f3b49b7bca7de31546cb0930ff23be96c579
mahan2103 <mahan.hs2103@gmail.com>
2026-05-20 10:09:39 +0200

    Create controller.cpp

---
+++ controller.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "controller.h"
+#include "device.h"
+
+using namespace std;
+
+void Controller::process(int lightLevel, bool motionDetected) {
+    Device device;
+
+    cout << "Light Level: " << lightLevel << endl;
+    cout << "Motion Detected: " << (motionDetected ? "YES" : "NO") << endl;
+
+    if (lightLevel < 40 && motionDetected) {
+        device.controlLight(true);
+    } else {
+        device.controlLight(false);
+    }
+
+    if (lightLevel > 60) {
+        device.controlFan(true);
+    } else {
+        device.controlFan(false);
+    }
+}
